549983ec780caa5fed5f7f3e78fd25d51ae981ef
Huy Le Hoang <ext-huy.leh@spdigital.sg>
2022-03-25 16:57:07 +0800

    Add WeatherApp project

---
+++ WeatherApp(MT)/Views/LoadingView.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingFiles.swift
+//  WeatherApp(MT)
+//
+//  Created by tuan.nguyen on 3/24/22.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
---
+++ WeatherApp(MT)/WeatherAppapp.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherAppapp.swift
+//  WeatherApp(MT)
+//
+//  Created by tuan.nguyen on 3/24/22.
+//
+
+import Foundation
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
